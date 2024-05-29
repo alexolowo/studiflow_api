e7234c388fbeed8fc48cb8ef0e359180b8d3c5f0
Alex Olowookere <alexandero@wingmateapp.com>
2024-05-29 10:05:46 -0400

    added file structure and got rid of garbage

--- Potential Startups/StudiFlow_V3/StudiFlowApi/user_courses/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import UserCourse
-
-class UserCoursesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserCourse
-        fields = '__all__'
--- Potential Startups/StudiFlow_V3/StudiFlowApi/user_courses/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user_courses/', views.get_user_courses, name='user_courses_list'),
-]
