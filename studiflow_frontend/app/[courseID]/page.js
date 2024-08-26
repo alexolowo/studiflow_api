b65123fb95071dc06801f586f31ab655533eabe6
Alex Olowookere <alexandero@wingmateapp.com>
2024-08-25 20:40:23 -0400

    inter course header navigation

--- studiflow_frontend/app/[courseID]/page.js
+++ studiflow_frontend/app/[courseID]/page.js
@@ -1,12 +1,13 @@
 'use client'
 
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import CourseSideNav from '@/components/CourseSideNav';
 import Chat from '@/components/chat';
 import TaskList from '@/components/taskList';
 import HeatMap from '@/components/heatMap';
-import { mapBackendFieldsToFrontendTask } from '@/lib/utils';
+import { mapBackendFieldsToFrontendTask, parseCourses } from '@/lib/utils';
+import { CourseHeader } from '@/components/courseHeader';
 
 
 export default function CourseView() {
@@ -61,6 +62,48 @@ export default function CourseView() {
         
     }, [taskChange]);
 
+    const [courseData, setCourseData] = useState([]);
+
+  useEffect(() => {
+    async function getCourses() {
+      try {
+        const accessToken = localStorage.getItem('accessToken');
+
+        const response = await fetch('http://localhost:8000/courses/', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.status === 401) {
+          // Remove tokens and redirect to home page
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('refreshToken');
+          router.push('/');
+          return;
+        }
+
+        if (!response.ok) {
+          setError('Failed getting active courses');
+          throw new Error(`HTTP error getting courses! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        data && setCourseData(parseCourses(data.courses));
+      } catch (e) {
+        setError(e.message);
+        console.error('There was a problem fetching the courses:');
+        console.error(e);
+      }
+    }
+
+    getCourses();
+  }, []);
+
+
 
     const renderContent = () => {
         switch (activeTab) {
@@ -76,6 +119,8 @@ export default function CourseView() {
                 return null;
         }
     };
+    
+    const chosenCourse = courseData.filter((course) => course.courseId == courseId);
 
     return (
         <div className="flex h-screen">
@@ -84,6 +129,7 @@ export default function CourseView() {
             <div className='flex items-center py-8 px-8 bg-gray-100 shadow-md text-2xl font-semibold text-gray-800'>
                     {/* TODO: make a dropdown to switch courses from here */}
                     {courseCode}
+                    <CourseHeader courses={courseData} currentCourse={chosenCourse} />
                 </div>
                 {renderContent()}
             </main>
---
+++ studiflow_frontend/components/courseHeader.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { ChevronDown } from 'lucide-react';
+
+export function CourseHeader({ courses, currentCourseName: currentCourse }) {
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const router = useRouter();
+
+  const handleCourseSelect = (course) => {
+    console.log(course);
+    setSelectedCourse(course);
+    const path = `/${course.courseId}-${course.courseCode}`;
+    router.push(path);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="flex items-center space-x-2">
+          {selectedCourse && currentCourse && (
+            <span className="text-2xl">
+              {selectedCourse ? selectedCourse.courseName : currentCourse.courseName}
+            </span>
+          )}
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {courses.map((course) => (
+          <DropdownMenuCheckboxItem key={course.id} onClick={() => handleCourseSelect(course)}>
+            {course.courseCode} - {course.courseName}
+          </DropdownMenuCheckboxItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
